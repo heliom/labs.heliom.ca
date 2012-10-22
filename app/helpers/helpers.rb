37b8b85a0b9a1cb3574731f87ead8eaea5cf505d
Etienne Lemay <etiennelemay21@gmail.com>
2012-10-22 17:12:29 -0400

    Generic content-box & add css/html source helpers

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # Assets tag helper
 # Uses sprockets in development and local precompiled files in production
 def javascript_include_tag(file_name)
@@ -20,4 +22,17 @@ end
 
 def url_for_view(view)
   view.gsub('_', '-')
+end
+
+# Asset links in #meta
+def asset_links(lab)
+  "#{github_css_link(lab)} · #{github_html_link(lab)}"
+end
+
+def github_css_link(lab)
+  %(<a href="https://github.com/heliom/labs.heliom.ca/blob/master/app/assets/stylesheets/labs/#{lab}.styl">css</a>)
+end
+
+def github_html_link(lab)
+  %(<a href="https://github.com/heliom/labs.heliom.ca/blob/master/app/views/labs/#{lab}.erb">html</a>)
 end
